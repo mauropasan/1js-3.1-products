49fa62636dff23ae3e411afd0543946148898487
Mauro Pastor Anduix <mauropasan01@gmail.com>
2022-10-05 19:23:05 +0200

    [FIX] More than 1 error message

--- src/view/view.class.js
+++ src/view/view.class.js
@@ -25,11 +25,13 @@ class View {
 
     renderMessage(err) {
         const messageUI = document.getElementById('messages');
-        messageUI.innerHTML = `
-        <div class="alert alert-danger alert-dismissible" role="alert">
+        const div = document.createElement("div");
+        div.classList = `alert alert-danger alert-dismissible`;
+        div.setAttribute(`role`, `alert`);
+        div.innerHTML += `
     		${err}
-	    	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onclick="this.parentElement.remove()"></button>
-		</div>`;
+	    	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onclick="this.parentElement.remove()"></button>`;
+        messageUI.appendChild(div);
     }
 
     renderCategory(category) {
